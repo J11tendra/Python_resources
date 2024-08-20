be18d19d30c83a0749d119e7e0151f5f1ea42dbc
Jitendra <info.jiitendra@gmail.com>
2024-08-20 12:04:45 +0530

    add: rps game

---
+++ rock-paper-scissor-game.py
@@ -0,0 +1,38 @@
+import sys, random
+from enum import Enum
+
+# Creating a rock, paper, scissor game
+
+class RPS(Enum):
+    ROCK = 1
+    PAPPER = 2
+    SCISSOR = 3
+
+print("")
+player_choice = input("Enter... \n1 for Rock, \n2 for Paper, \n3 for Scissors:\n\n")
+
+player = int(player_choice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1, 2, or 3.")
+
+computer_choice = random.choice("123")
+computer = int(computer_choice)
+
+
+print("")
+print("You chose: " + str(RPS(player)).replace("RPS.", "") + ".")
+print("Computer chose: " + str(RPS(computer)).replace("RPS.", "") + ".")
+print("")
+
+
+if player == 1 and computer == 3:
+    print("Yay, You Win!")
+elif player == 2 and computer == 1:
+    print("Yay, You Win!")
+elif player == 3 and computer == 2:
+    print("Yay, You Win!")
+elif player == computer:
+    print("It's a Tie Game.")
+else:
+    print("ohho, Computer Wins!")
