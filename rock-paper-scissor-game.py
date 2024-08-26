36c2f7653c748b823805d4be2e6974d99eb41bf2
Jitendra <info.jiitendra@gmail.com>
2024-08-26 18:14:34 +0530

    refactor: make winning precise

--- rock-paper-scissor-game.py
+++ rock-paper-scissor-game.py
@@ -13,7 +13,7 @@ player_choice = input("Enter... \n1 for Rock, \n2 for Paper, \n3 for Scissors:\n
 
 player = int(player_choice)
 
-if player < 1 | player > 3:
+if player < 1 or player > 3:
     sys.exit("You must enter 1, 2, or 3.")
 
 computer_choice = random.choice("123")
