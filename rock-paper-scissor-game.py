5adc8050ad755452d9b3a2bf185db1ea9b39c927
Jitendra <info.jiitendra@gmail.com>
2024-08-31 10:26:51 +0530

    refactor: add recursion calls

--- rock-paper-scissor-game.py
+++ rock-paper-scissor-game.py
@@ -3,28 +3,29 @@ from enum import Enum
 
 # Creating a rock, paper, scissor game
 
-class RPS(Enum):
-    ROCK = 1
-    PAPPER = 2
-    SCISSOR = 3
 
-playagain = True
+def play_rps():
 
-while playagain:
-    player_choice = input("\nEnter... \n1 for Rock, \n2 for Paper, \n3 for Scissors:\n\n")
+    class RPS(Enum):
+        ROCK = 1
+        PAPPER = 2
+        SCISSOR = 3
+
+    player_choice = input(
+        "\nEnter... \n1 for Rock, \n2 for Paper, \n3 for Scissors:\n\n"
+    )
+
+    if player_choice not in ["1", "2", "3"]:
+        print("You must enter 1, 2, or 3.")
+        return play_rps()
 
     player = int(player_choice)
-
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1, 2, or 3.")
 
     computer_choice = random.choice("123")
     computer = int(computer_choice)
 
-
     print("\nYou chose: " + str(RPS(player)).replace("RPS.", "") + ".")
     print("Computer chose: " + str(RPS(computer)).replace("RPS.", "") + ".\n")
-
 
     if player == 1 and computer == 3:
         print("🏆🏆🏆Yay, You Win!")
@@ -37,13 +38,21 @@ while playagain:
     else:
         print("🖥🖥🖥ohho, Computer Wins!")
 
-    playagain = input('\nPlay again? \nY for Yes or \nQ to Quit\n\n')
+    print("\nPlay again?")
+
+    while True:
+        playagain = input("\nY for Yes or \nQ to Quit\n\n")
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
 
     if playagain.lower() == "y":
-        continue
+        return play_rps()
     else:
         print("\n🪅 🎊 🎉 🪅 🎊 🎉 🪅 🎊 🎉")
         print("It wouldn't be the same without you")
-        playagain = False
+        sys.exit("\n\n........Thanks you playing.......\n\n")
 
-sys.exit("\n\n........Thanks you playing.......\n\n")
+
+play_rps()
